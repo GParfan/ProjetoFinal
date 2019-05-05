755ff25acea8412ff5cf649847551b02cef271d7
GParfan <49314033+GParfan@users.noreply.github.com>
2019-05-04 22:16:28 -0300

    Speedx fixed

--- CodigoPF.py
+++ CodigoPF.py
@@ -3,8 +3,8 @@ from os import path
 
 img_dir = path.join(path.dirname(__file__),'img')
 
-WIDTH = 1200
-HEIGHT = 800
+WIDTH = 995
+HEIGHT = 654
 FPS = 60
 
 WHITE = (255, 255, 255)
@@ -17,34 +17,37 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         
-        personagem = pygame.image.load(path.join(img_dir,"PersonagemTeste.png")).convert()
+        personagem = pygame.image.load(path.join(img_dir,"Personagemteste.png")).convert_alpha()
         
         self.image = personagem
         
         self.image = pygame.transform.scale(personagem,(70,100))
         
-        self.image.set_colorkey(GREEN)
+        self.image.set_colorkey(BLACK)
         
         self.rect = self.image.get_rect()
         
         self.rect.centerx = WIDTH/2
-        self.rect.bottom = HEIGHT - 150
+        self.rect.bottom = HEIGHT - 87
         
         self.speedx = 0
         self.speedy = 0
         
     def update(self):
+        
         self.rect.x += self.speedx
-        self.rect.y += self.speedy
         
         if self.rect.right > WIDTH:
             self.rect.right = WIDTH
         if self.rect.left < 0:
             self.rect.left = 0
-        if self.rect.up > 0:
-            self.rect.up = 0
-        if self.rect.down < HEIGHT:
-            self.rect.down = HEIGHT
+        
+        self.rect.y += self.speedy
+        
+        if self.rect.top < 0:
+            self.rect.top = 0
+        if self.rect.bottom > HEIGHT - 87:
+            self.rect.bottom = HEIGHT - 87
     
 pygame.init()
 pygame.mixer.init()
@@ -54,7 +57,7 @@ pygame.display.set_caption("Personagem")
 
 clock = pygame.time.Clock()
 
-background = pygame.image.load(path.join(img_dir,"cenarioteste.png"))
+background = pygame.image.load(path.join(img_dir,"Imagem de fundo.jpg"))
 background_rect = background.get_rect()
 
 player = Player()
@@ -73,26 +76,29 @@ try:
             if event.type == pygame.QUIT:
                 running = False
             
-#            if event.type == pygame.KEYDOWN:
-#                if event.key == pygame.K_LEFT:
-#                    player.speedx = -10
-#                if event.key == pygame.K_RIGHT:
-#                    player.speedx = 10
-#            if event.type == pygame.KEYUP:
-#                if event.key == pygame.K_LEFT:
-#                    player.speedx = 0
-#                if event.key == pygame.K_RIGHT:
-#                    player.speedx = 0
-#                    
-#            if event.type == pygame.KEYUP:
-#               if event.key == pygame.K_LEFT:
-#                    player.speedy = -20
-#                if event.key == pygame.K_UP:
-#                    player.speedy = 20
-#        
-#        all_sprites.update()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    player.speedx = -10
+                if event.key == pygame.K_RIGHT:
+                    player.speedx = 10
+                if event.key == pygame.K_UP:
+                    player.speedy = -10
+                if event.key == pygame.K_DOWN:
+                    player.speedy = 10
+                    
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_DOWN:
+                    player.speedy = 0
+                if event.key == pygame.K_UP:
+                    player.speedy = 0
+                if event.key == pygame.K_RIGHT:
+                    player.speedx = 0
+                if event.key == pygame.K_LEFT:
+                    player.speedx = 0
+
+        all_sprites.update()
                
-        screen.fill(GREEN)
+        screen.fill(BLACK)
         screen.blit(background, background_rect)
         all_sprites.draw(screen)
         pygame.display.flip()
@@ -100,19 +106,4 @@ try:
 finally:
     pygame.quit()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
